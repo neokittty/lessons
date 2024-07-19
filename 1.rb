a732852ca4a99e50ad5cdfadc004991cb328c519
neokittty <sydorenkodara@gmail.com>
2024-07-19 21:54:23 +0300

    gets

---
+++ 1.rb
@@ -0,0 +1,14 @@
+# puts "who are you?"
+# name = gets.chomp
+# puts 'Hi, " + name + ", how are you?'
+# puts "Hi, #{name}, how are you?"
+# puts "Hi, ".concat(name).concat(', how are you?')
+
+# .to_i
+# .to_s
+# .to_a
+# .to_f
+
+puts "how?"
+name = gets.chomp.to_i
+puts name * 5
