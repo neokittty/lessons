b0b2e2bae9c66f19a2180c781a7905916237d24b
neokittty <sydorenkodara@gmail.com>
2024-07-23 22:17:22 +0300

    array name

---
+++ array_name.rb
@@ -0,0 +1,15 @@
+man = ['Romo4ka', 'Roman', 'Roma']
+woman = ['Lina', 'Kate', 'Ann']
+p man
+p woman
+
+p man.to_s + woman.to_s
+
+p man + woman
+
+p "#{man} #{woman}"
+
+p man.concat(woman)
+
+p man << woman
+
